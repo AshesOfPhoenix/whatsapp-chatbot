7ad12d58a83e6e42da49b71354d560bcfd940363
Kristjan Križman <kristjan.krizman@gmail.com>
2025-02-25 20:44:11 +0100

    refactor: extract root greeting constant and update tests

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,9 @@
-import { Injectable } from '@nestjs/common'
+import { Injectable } from '@nestjs/common';
+import { rootGreeting } from './utils/constants';
 
 @Injectable()
 export class AppService {
-    getHello(): string {
-        return 'Hello World!'
-    }
+  getHello(): string {
+    return rootGreeting;
+  }
 }
---
+++ src/utils/constants.ts
@@ -0,0 +1,23 @@
+export const rootGreeting = `
+<style>
+body {
+    font-family: Arial, sans-serif;
+    text-align: center;
+}
+h1 {
+    font-size: 40px;
+    color: #333;
+}
+p {
+    font-size: 20px;
+    color: #666;
+}
+a {
+    text-decoration: none;
+    color: #337ab7;
+}
+</style>
+<h1>Welcome to the Notefy Whatsapp Bot!</h1>
+<p>This is a simple API that will send a message with the information from the Notefy transcription to a Coda.io note.</p>
+<p>Check out the documentation: <a href="/docs">/docs</a></p>
+`;
